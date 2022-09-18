086e35d0d4d66c6b0d8bf0d4175797edfc21cc30
puxxbu <puxxbu@users.noreply.github.com>
2022-09-18 23:52:56 +0700

    Initial commit

---
+++ process/registerProcess.php
@@ -0,0 +1,39 @@
+<?php
+    include('../db.php');
+    if (isset($_POST['register'])) {
+
+
+        $email = $_POST['email'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $name = $_POST['name'];
+        $phonenum = $_POST['phonenum'];
+        $membership = $_POST['membership'];
+
+        $query = mysqli_query($con,
+            "INSERT INTO users(email, password, name, phonenum, membership)
+                    VALUES  ('$email', '$password', '$name', '$phonenum', '$membership')")
+            or die(mysqli_error($con));
+
+        if($query){
+            echo
+            '<script>
+            alert("Register Success");
+            window.location = "../index.php"
+            </script>';
+        }else{
+            echo
+            '<script>
+            alert("Register Failed");
+            </script>';
+        }
+    }else{
+        echo
+        '<script>
+        window.history.back()
+        </script>';
+    }
+
+
+
+
+
